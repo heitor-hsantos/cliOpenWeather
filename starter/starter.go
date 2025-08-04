f93d534d26f6980710c710a5d83efe8ab3317bb4
heitor-hsantos <heitor.santos118@gmail.com>
2025-08-04 11:16:38 -0300

    feat:initial refactor of starter.go migrating CLI comands to apropriated location

--- starter/starter.go
+++ starter/starter.go
@@ -12,6 +12,7 @@ import (
 )
 
 func HandleCLI() {
+
 	if len(os.Args) < 3 || os.Args[1] != "get" || os.Args[2] != "weather" {
 		fmt.Println("Uso: go run main.go get weather \n" +
 			"<Se o CLI não for configurdao previamente o comando exibira por default dados de Nova York>")
